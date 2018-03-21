0d39d7896c926d6d2c62cb16b7325700e364f9ad
80002755 <80002755@sf-express.com>
2018-03-21 19:08:57 +0800

    pdf-vue

--- src/components/SProject.vue
+++ src/components/SProject.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="s-project">
     <fix-til></fix-til>
+    <pdf src="http://cdn.mozilla.net/pdfjs/tracemonkey.pdf"></pdf>
   </div>
 </template>
 <script>
 import FixTil from './FixTil.vue'
+import pdf from 'vue-pdf'
 
 export default {
   name: 'SProject',
   components: {
-    FixTil
+    FixTil,
+    pdf
   },
   data () {
     return {
--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import App from './App'
 import router from './router'
 import Mint from 'mint-ui'
 import store from '@/store'
-import pdf from './tool/pdf'
+// import pdf from './tool/pdf'
 
 Vue.use(Mint)
-Vue.use(pdf)
+// Vue.use(pdf)
 
 Vue.config.productionTip = false
 
